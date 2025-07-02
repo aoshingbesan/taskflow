1a35b88543045df0ccca08183018e5714789976a
Ademola <a.oshingbes@alustudent.com>
2025-07-02 22:59:20 +0200

    Assignment Completed

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+ 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
